6774b4f5a00bd4848660756ee1a17491b3d9bf5a
Tatsuki Terao <tterao@c3r2s1.42tokyo.jp>
2024-07-06 22:36:50 +0900

    :seedling:test: ex16

--- ex16/main.go
+++ ex16/main.go
@@ -5,6 +5,13 @@ import (
 	"piscine"
 )
 
+const (
+	UintMax = ^uint(0)
+	UintMin = 0
+	IntMax  = int(UintMax >> 1)
+	IntMin  = -IntMax - 1
+)
+
 func main() {
 	piscine.PrintNbrBase(125, "0123456789")
 	ft.PrintRune('\n')
@@ -15,4 +22,9 @@ func main() {
 	piscine.PrintNbrBase(-125, "choumi")
 	ft.PrintRune('\n')
 	piscine.PrintNbrBase(125, "aa")
+	ft.PrintRune('\n')
+	piscine.PrintNbrBase(IntMax, "0123456789ABCDEF")
+	ft.PrintRune('\n')
+	piscine.PrintNbrBase(IntMin, "0123456789ABCDEF")
+	ft.PrintRune('\n')
 }
